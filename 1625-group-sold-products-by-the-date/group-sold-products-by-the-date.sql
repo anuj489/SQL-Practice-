290d4ae9cd67c96673aab0eb55b4b2fc127a67d3
Anuj Agarwal <90152355+anuj489@users.noreply.github.com>
2025-07-31 01:12:24 +0530

    Time: 560 ms (12.46%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1625-group-sold-products-by-the-date/group-sold-products-by-the-date.sql
@@ -0,0 +1,8 @@
+SELECT 
+    sell_date,
+    COUNT(distinct(product)) AS num_sold,
+    GROUP_CONCAT(distinct product ORDER BY product SEPARATOR ',') AS products
+FROM 
+    Activities
+GROUP BY 
+    sell_date;
